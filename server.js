02967c23b9e95f2b052c37b56e15dcad0d44e76a
Nedzmija <nedzmijakaric6@gmail.com>
2023-05-16 22:29:59 +0200

    first try of get calls

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const basicAuth = require('express-basic-auth');
 const fs = require('fs');
 
+const apiKey = 'API_KEY';
 const app = express();
 
 app.use(
@@ -17,5 +18,31 @@ app.use(
   
   app.listen(3000, () => {
     console.log('Server is running on port 3000');
+  });
+
+// Endpoint for get current weather conditions
+app.get('/weather/current', async (req, res) => {
+    try {
+      const { lokacija } = req.query;
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${lokacija}&appid=${apiKey}`);
+      const currWeather = response.data;
+      res.json(currWeather);
+    } catch (error) {
+      console.error(error);
+      res.status(400).json({ error: 'Failed' });
+    }
+  });
+
+  // Endpoint fr get weather forecast
+app.get('/weather/forecast', async (req, res) => {
+    try {
+      const { lokacija } = req.query;
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${lokacija}&appid=${apiKey}`);
+      const weatherForecast = response.data;
+      res.json(weatherForecast);
+    } catch (error) {
+      console.error(error);
+      res.status(400).json({ error: 'Failed' });
+    }
   });
   
